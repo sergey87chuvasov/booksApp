eb1f38328b62720741552059b8e173a14774e3be
Sergey Chv <serge.techno@gmail.com>
2023-05-22 22:48:57 +0300

    added state and reactivity

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ class App {
   // public property
   routes = [{ path: '', view: MainView }];
 
+  // state
+  appState = {
+    favorites: []
+  }
+
   constructor() {
     window.addEventListener('hashchange', this.route.bind(this));
     this.route();
@@ -15,7 +20,7 @@ class App {
     }
 
     const view = this.routes.find((r) => r.path == location.hash).view;
-    this.currentView = new view();
+    this.currentView = new view(this.appState);
     this.currentView.render();
   }
 }
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,33 @@
 import { AbstractView } from '../../common/view.js';
+import onChange from 'on-change';
 
 export class MainView extends AbstractView {
-  constructor() {
+  state = {
+    list: [],
+    loading: false,
+    searchQuery: undefined,
+    offset: 0
+  }
+
+  constructor(appState) {
     super();
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.appStateHook.bind(this))
     this.setTitle('Поиск книг');
+  }
+
+  appStateHook(path) {
+    console.log(path);
+    if (path === 'favorites') {
+      this.render()
+    }
   }
 
   render() {
     const main = document.createElement('div');
-    main.innerHTML = 'Тест';
+    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
     this.app.innerHTML = '';
     this.app.append(main);
+    this.appState.favorites.push('d')
   }
 }
